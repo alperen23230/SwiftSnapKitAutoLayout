3e6fcb6d882a87b02da774fca6b2f3b947fa76cc
Alperen Ünal <alperenunal@alperens-macbook-air.local>
2019-01-23 21:09:17 +0300

    Initial Commit

---
+++ AutoLayoutTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutTraining
+//
+//  Created by Alperen Ünal on 23.01.2019.
+//  Copyright © 2019 Alperen Ünal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
